295674911e7e7bb34eb49acd46659a2f80875a1e
Ryoka <tenkaro@icloud.com>
2026-07-13 19:26:06 +0800

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from "next";
+
+const BASE_URL = "https://pieterborremans.com";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: `${BASE_URL}/sitemap.xml`,
+  };
+}
